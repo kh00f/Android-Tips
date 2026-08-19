6124c58136bb22e9357acd6a6d768475b5865909
Yassin El Feki <126656589+kh00f@users.noreply.github.com>
2026-08-19 08:23:35 -0700

    Update GET THE BuildConfig FILES.sh

--- GET THE BuildConfig FILES.sh
+++ GET THE BuildConfig FILES.sh
@@ -1,1 +1,12 @@
 find . \( -name "BuildConfig.java" -o -name "BuildConfig.smali" \)
+
+------------------------------------
+
+find . \( -name "BuildConfig.java" -o -name "BuildConfig.smali" \) \
+-exec sh -c '
+for f; do
+    echo "========== $f =========="
+    cat "$f"
+    echo
+done
+' sh {} +
